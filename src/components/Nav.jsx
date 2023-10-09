3256b5ba3d5b227cefd982edc7be6bf4f02785a3
Quartezz <kamil.machel03@gmail.com>
2023-10-09 17:35:10 +0200

    styled hero section

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -20,7 +20,7 @@ function Nav() {
             </li>
           ))}
         </ul>
-        <div className="hidden max-lg: block">
+        <div className="hidden max-lg:block">
           <img src={hamburger} alt="Hamburger" width={25} height={25} />
         </div>
       </nav>
